72e39287de336172620ead32d0901fcb1e7721a9
dionisius <denantastar@gmail.com>
2025-06-18 14:45:41 +0700

    perubahan sedikit

--- js/script.js
+++ js/script.js
@@ -1,31 +1,10 @@
-// Toggle the visibility of a dropdown menu
-const toggleDropdown = (dropdown, menu, isOpen) => {
-  dropdown.classList.toggle("open", isOpen);
-  menu.style.height = isOpen ? `${menu.scrollHeight}px` : 0;
-};
-
-// Close all open dropdowns
+// KECILIN NAVBAR
 const closeAllDropdowns = () => {
   document.querySelectorAll(".dropdown-container.open").forEach((openDropdown) => {
     toggleDropdown(openDropdown, openDropdown.querySelector(".dropdown-menu"), false);
   });
 };
 
-// Attach click event to all dropdown toggles
-document.querySelectorAll(".dropdown-toggle").forEach((dropdownToggle) => {
-  dropdownToggle.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    const dropdown = dropdownToggle.closest(".dropdown-container");
-    const menu = dropdown.querySelector(".dropdown-menu");
-    const isOpen = dropdown.classList.contains("open");
-
-    closeAllDropdowns(); // Close all open dropdowns
-    toggleDropdown(dropdown, menu, !isOpen); // Toggle current dropdown visibility
-  });
-});
-
-// Attach click event to sidebar toggle buttons
 document.querySelectorAll(".sidebar-toggler, .sidebar-menu-button").forEach((button) => {
   button.addEventListener("click", () => {
     closeAllDropdowns(); // Close all open dropdowns
@@ -33,10 +12,10 @@ document.querySelectorAll(".sidebar-toggler, .sidebar-menu-button").forEach((but
   });
 });
 
-// Collapse sidebar by default on small screens
-if (window.innerWidth <= 1024) document.querySelector(".sidebar").classList.add("collapsed");
+// COLLAPSE DI SCREEN KECIL
+if (window.innerWidth <= 2000) document.querySelector(".sidebar").classList.add("collapsed");
 
-// Enable horizontal scrolling with vertical scroll input
+// SCROLL SAMPING
 document.addEventListener('wheel', (event) => {
   if (event.deltaY !== 0) {
     event.preventDefault(); // Prevent default vertical scrolling
